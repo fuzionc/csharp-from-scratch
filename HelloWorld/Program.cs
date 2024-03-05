d4e9d35cbe5073e5fbe545d5dc4a741d295a45b2
fuzionc <betancurtc110@gmail.com>
2024-03-05 09:27:00 -0500

    Change hello world by class

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿namespace HelloWorld{
+    class Program{
+        static void Main(){
+            string Name="Jhon";
+            Console.WriteLine($"Hello World! {Name}");
+            Console.WriteLine();
+        }
+    }
+}
